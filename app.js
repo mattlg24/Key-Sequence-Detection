76d054fe515f18d8c2b866186821ef5dcfeb1803
Matt Gardner <matthewlgardner@gmail.com>
2017-05-08 14:24:46 -0600

    log secret code

--- app.js
+++ app.js
@@ -3,4 +3,13 @@ window.onload = function() {
     let pressed = []
     let secretCode = 'lemmon'
 
+    window.addEventListener('keyup', function(e) {
+      pressed.push(e.key)
+      pressed.splice(-secretCode.length - 1, pressed.length - secretCode.length)
+      console.log(pressed.join(''));
+
+      if (pressed.join('') === secretCode) {
+      
+      }
+    })
 }
